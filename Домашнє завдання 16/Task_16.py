365fc441f95518958029ceaa9a1140304b41da41
Olha Rybak <rybak.dtl@gmail.com>
2023-03-25 16:36:13 +0200

    Number

--- Домашнє завдання 16/Task_16.py
+++ Домашнє завдання 16/Task_16.py
@@ -3,9 +3,9 @@ import json
 my_dict = {
     345643: ("Alice", 15),
     786547: ("Tracy", 16),
-    376548: ("Serge", 40),
+    376548: ("Serge", 20),
     345619: ("David", 20),
-    456728: ("Michael", 35),
+    456728: ("Michael", 25),
 	 675489: ("Andrew", 19)
 }
 
